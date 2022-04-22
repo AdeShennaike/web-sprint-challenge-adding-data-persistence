8679158edac313648c78ab4418e7c10294e4e9cd
Ade <shennaike2386@hotmail.com>
2022-04-22 15:42:42 -0400

    resource model and endpoints done

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,19 @@
 // build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+async function create(resource) {
+    const rows = db('resources').insert(resource)
+    return rows
+}
+
+async function find() {
+    const rows = await db('resources')
+    .select('resources.*')
+
+    return rows
+}
+
+module.exports = {
+    create,
+    find
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,6 +1,26 @@
 // build your `/api/resources` router here
 const express = require('express')
-
+const resources = require('./model')
 const router = express.Router()
+
+router.get('/', (req, res) => {
+    resources.find()
+        .then(resource => {
+            res.status(200).json(resource)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+router.post('/', async (req, res) => {
+    
+    try{
+        const newResource = await resources.create(req.body)
+            res.status(201).json(newResource)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
