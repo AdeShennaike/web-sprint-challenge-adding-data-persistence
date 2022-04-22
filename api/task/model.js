176fa6f4ea76415dce400bc1ba15faad90afdb6b
Ade <shennaike2386@hotmail.com>
2022-04-22 16:01:13 -0400

    tasks model and endpoints done

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,19 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig')
+
+async function create(project) {
+    const rows = db('tasks').insert(project)
+    return rows
+}
+
+async function find() {
+    const rows = await db('tasks')
+    .select('tasks.*')
+
+    return rows
+}
+
+module.exports = {
+    create,
+    find
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,6 +1,25 @@
 // build your `/api/tasks` router here
 const express = require('express')
-
+const tasks = require('./model')
 const router = express.Router()
+
+router.get('/', (req, res) => {
+    tasks.find()
+        .then(task => {
+            res.status(200).json(task)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+router.post('/', async (req, res) => {
+    try{
+        const newTasks = await tasks.create(req.body)
+        res.status(201).json(newTasks)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
