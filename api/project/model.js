db5c9ab6d4b4b6dfcedad7ef49301c75b181cc9a
Ade <shennaike2386@hotmail.com>
2022-04-22 15:47:51 -0400

    projects model and endpoints done

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,19 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+async function create(project) {
+    const rows = db('projects').insert(project)
+    return rows
+}
+
+async function find() {
+    const rows = await db('projects')
+    .select('projects.*')
+
+    return rows
+}
+
+module.exports = {
+    create,
+    find
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,6 +1,25 @@
 // build your `/api/projects` router here
 const express = require('express')
-
+const projects = require('./model')
 const router = express.Router()
+
+router.get('/', (req, res) => {
+    projects.find()
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+router.post('/', async (req, res) => {
+    try{
+        const newProjects = await projects.create(req.body)
+        res.status(201).json(newProjects)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
