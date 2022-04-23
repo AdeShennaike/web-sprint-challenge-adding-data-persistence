b1964f7ff8b0612204b4fb85e1d0b9dd97a54fc4
Ade <shennaike2386@hotmail.com>
2022-04-22 20:40:45 -0400

    all test except 13  pass

--- api/task/model.js
+++ api/task/model.js
@@ -1,17 +1,53 @@
 // build your `Task` model here
 const db = require('../../data/dbConfig')
 
+async function getById(id) {
+    const rows = await db("tasks").where("task_id", id).first();
+    rows.task_completed === 0 || !rows.task_completed
+      ? (rows.task_completed = false)
+      : (rows.task_completed = true);
+    return rows;
+  }
+  
 async function create(project) {
-    const rows = db('tasks').insert(project)
-    return rows
+    // const [task_id] = db('tasks').insert(project)
+    // return getById(task_id)
+
+     await db('tasks')
+        .insert(project)
+    const newTask = await db('tasks')
+        .where('task_description', project.task_description)
+        .first()
+
+        if(newTask.task_completed === 0){
+            return {
+                ...newTask,
+                task_completed: false
+            }
+        } else {
+            return {
+                ...newTask,
+                task_completed: true
+            }
+        }
 }
 
 async function find() {
-    const rows = await db('tasks')
-    .select('tasks.*')
+    const rows = await db('tasks as t')
+    .leftJoin("projects as p", "p.project_id", "t.project_id")
+    .select(
+      "t.task_id",
+      "t.task_completed",
+      "t.task_notes",
+      "t.task_description",
+      "p.project_description",
+      "p.project_name")
 
-    return rows
-}
+    rows.map(row => {
+        row.task_completed = !!row.task_completed;
+        return row;
+    })
+    return rows}
 
 module.exports = {
     create,
